203b29752280a1d8874a566c0c8a568278bfe75f
wuxigee <575298369@qq.com>
2019-04-18 16:17:09 +0800

    五

---
+++ ClosePortsGadgets/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace 关闭端口小工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolbtnClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClosePoint = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "获取开启端口的程序";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(3, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(575, 292);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolbtnClose});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(227, 26);
+            // 
+            // toolbtnClose
+            // 
+            this.toolbtnClose.Name = "toolbtnClose";
+            this.toolbtnClose.Size = new System.Drawing.Size(226, 22);
+            this.toolbtnClose.Text = "根据PID获取开启端口的程序";
+            this.toolbtnClose.Click += new System.EventHandler(this.toolbtnClose_Click);
+            // 
+            // btnClosePoint
+            // 
+            this.btnClosePoint.Location = new System.Drawing.Point(244, 6);
+            this.btnClosePoint.Name = "btnClosePoint";
+            this.btnClosePoint.Size = new System.Drawing.Size(129, 23);
+            this.btnClosePoint.TabIndex = 4;
+            this.btnClosePoint.Text = "关闭端口";
+            this.btnClosePoint.UseVisualStyleBackColor = true;
+            this.btnClosePoint.Click += new System.EventHandler(this.btnClosePoint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 342);
+            this.Controls.Add(this.btnClosePoint);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolbtnClose;
+        private System.Windows.Forms.Button btnClosePoint;
+    }
+}
+
---
+++ ClosePortsGadgets/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+// 下载于www.mycodes.net
+namespace 关闭端口小工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Process p;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMsgToCMD(string.Format("netstat -aon|findstr {0}", textBox1.Text));
+        }
+
+        private void SendMsgToCMD(string CMD)
+        {
+            p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            //要执行的程序名称
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            //可能接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;
+            //由调用程序获取输出信息
+            p.StartInfo.CreateNoWindow = true;
+            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+            p.Start();//启动程序
+            //向CMD窗口发送输入信息：
+            p.StandardInput.WriteLine(CMD);
+            p.BeginOutputReadLine();
+        }
+
+        void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                StringBuilder sb = new StringBuilder(this.textBox1.Text);
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    this.listBox1.Items.Add(e.Data.ToString());
+
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                }));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (p != null)
+                p.Close();
+        }
+
+        private void toolbtnClose_Click(object sender, EventArgs e)
+        {
+            string tmpmsg = listBox1.SelectedItem.ToString();
+            string CMDMsg = tmpmsg.Substring(tmpmsg.LastIndexOf(' '));
+            SendMsgToCMD(string.Format("tasklist|findstr {0}", CMDMsg));
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (listBox1.Items == null || listBox1.SelectedItem == null) return;
+
+            if (e.Button == MouseButtons.Right
+               && (listBox1.SelectedItem.ToString().Contains("TCP")
+               || listBox1.SelectedItem.ToString().Contains("UDP")))
+                listBox1.ContextMenuStrip = contextMenuStrip1;
+            else
+                listBox1.ContextMenuStrip = null;
+        }
+
+        /**0
+         * 关闭端口
+         * **/
+        private void btnClosePoint_Click(object sender, EventArgs e)
+        {
+            string tmpmsg = listBox1.SelectedItem.ToString();
+            string CMDMsg = tmpmsg.Substring(tmpmsg.LastIndexOf (' '));
+            SendMsgToCMD(string.Format("taskkill /f /t /im {0}", CMDMsg));
+        }
+
+    }
+}
